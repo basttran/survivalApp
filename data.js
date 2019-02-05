7378b85287bbdfc3f002f0d325e79f98dbdbbdef
Estelle Olawinski <estelle.olawinski@me.com>
2019-02-05 21:56:07 +0100

    coucou

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+
+        name: "Bleuet",
+        scientificName: "Cyranus segetum, Centaurea cyanus",
+        description: "Le bleuet est utilisé depuis fort longtemps, en phytothérapie, pour lutter contre les irritations des yeux et des paupières ou les conjonctivites et, plus généralement, pour apaiser de nombreuses inflammations de la peau et des muqueuses. Il est également utilisé pour ses vertus diurétiques.",
+        aliases: "bleuet, centaurée bleuet, audifoin, barbeau, blavelle, bleuet des champs, casse-lunettes, fleur de Zacharie.",
+        botaniqueClass: "famille des astéracées (Asteraceae)",
+        image: "https://images.media-allrecipes.com/images/75131.jpg",
+        applications: ["Medicine"],
+        history: "Le bleuet est une plante originaire du Proche-Orient, qui s'est largement répandue à travers le monde. Ses vertus médicinales sont mentionnées pour la première fois dans un ouvrage du XIIe siècle. Sa couleur bleue symbolisait les yeux sains et le bleuet servait donc de remède contre les différentes affections oculaires. Cette propriété est aujourd'hui contestée. Autrefois, le bleuet était utilisé pour soigner les yeux bleus, alors que les yeux marron ou noirs étaient traités grâce au plantain.",
+        internUsage: "Diurétique. Stimulant du métabolisme digestif. Cholagogue léger. Expectorant. Anti-inflammatoire, décongestionnant des bronches.",
+        externUsage: "Calmant ophtalmique.",
+        phytochemicalBasis: ["Alkaloids", "Glycosides", "Polyphenols", "Terpenes"],
+        calendar: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        part: {type: String, enum: ["leave","shoot","root","bark","wood","nuts","seed","fruit"],
+        usage: "décoctions, tisanes, infusions, collyres, bains"
+  },
+  {
+    title: "Orange and Milk-Braised Pork Carnitas",
+    level: "UltraPro Chef",
+    ingredients: [
+      "3 1/2 pounds boneless pork shoulder, cut into large pieces",
+      "1 tablespoon freshly ground black pepper",
+      "1 tablespoon kosher salt, or more to taste",
+      "2 tablespoons vegetable oil",
+      "2 bay leaves",
+      "2 teaspoons ground cumin",
+      "1 teaspoon dried oregano",
+      "1/4 teaspoon cayenne pepper",
+      "1 orange, juiced and zested"
+    ],
+    cuisine: "American",
+    dishType: ["Dish"],
+    image: "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+    duration: 160,
+    creator: "Chef John"
+  },
+];
