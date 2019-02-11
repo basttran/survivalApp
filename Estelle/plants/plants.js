7996237af6a6a08915ea93ac7b7970180ace68c6
Estelle Olawinski <estelle.olawinski@me.com>
2019-02-11 11:36:05 +0100

    database fnished, bastien is it ok for you?

--- Estelle/plants/plants.js
+++ Estelle/plants/plants.js
@@ -1,196 +1,21 @@
 {
-    varieties: [ ],
-    // sub_species: [ ],
-    scientific_name: "Cactus funalis",
-    order: {
-    slug: "caryophyllales",
-    name: "Caryophyllales",
-    // link: "http://trefle.io/api/division_orders/6",
-    // id: 6
-    },
-    // native_status: null,
-    main_species: {
-    year: null,
-    type: "species",
-    synonym: true,
-    status: "Synonym",
-    specifications: {
-    toxicity: null,
-    shape_and_orientation: null,
-    regrowth_rate: null,
-    nitrogen_fixation: null,
-    max_height_at_base_age: {
-    ft: null,
-    cm: null
-    },
-    mature_height: {
-    ft: null,
-    cm: null
-    },
-    low_growing_grass: null,
-    lifespan: null,
-    leaf_retention: null,
-    known_allelopath: null,
-    growth_rate: null,
-    growth_period: null,
-    growth_habit: null,
-    growth_form: null,
-    fire_resistance: null,
-    fall_conspicuous: null,
-    coppice_potential: null,
-    c_n_ratio: null,
-    bloat: null
-    },
-    sources: [
-    {
-    species_id: 295486,
-    source_url: "http://www.tropicos.org",
-    name: "Tropicos",
-    last_update: "2019-01-12T13:15:39.966285"
-    }
+    "name": "Bunny Ears Cactus",
+    "scientificName": "Opuntia microdasys",
+    "commonNames": "Bunny Ears, Bunny Ears Cactus, Bunny Cactus, Polka Dot Cactus, Bunny-ears Prickly-pear, Angel’s Wings, Golden-Bristle, Golden-Bristle Cactus",
+    "place": "indoor",
+    "description": "Originally from Mexico, the bunny ears cactus is named after its appearance. It has two pads that are bunny ear shaped. They are covered with glochids or brown prickles and should be handled with care. The bunny ears cactus grows to two or three feet, making it the perfect house plant. It produces white flowers and purple fruits in the summer if exposed to enough light.",
+    "scientificClassification": [
+      { "Family": "Cactaceae" },
+      { "Subfamily": "Opuntioidea" },
+      { "Tribe": "Opuntieae" },
+      { "Genus": "Opuntia" }
     ],
-    soils_adaptation: {
-    medium: null,
-    fine: null,
-    coarse: null
-    },
-    slug: "cactus-funalis",
-    seed: {
-    vegetative_spread_rate: null,
-    small_grain: null,
-    seeds_per_pound: null,
-    seedling_vigor: null,
-    seed_spread_rate: null,
-    commercial_availability: null,
-    bloom_period: null
-    },
-    scientific_name: "Cactus funalis",
-    propagation: {
-    tubers: null,
-    sprigs: null,
-    sod: null,
-    seed: null,
-    cuttings: null,
-    corms: null,
-    container: null,
-    bulbs: null,
-    bare_root: null
-    },
-    products: {
-    veneer: null,
-    pulpwood: null,
-    protein_potential: null,
-    post: null,
-    palatable_human: null,
-    palatable_graze_animal: null,
-    palatable_browse_animal: null,
-    nursery_stock: null,
-    naval_store: null,
-    lumber: null,
-    fuelwood: null,
-    fodder: null,
-    christmas_tree: null,
-    berry_nut_seed: null
-    },
-    native_status: null,
-    main_species_id: null,
-    is_main_species: true,
-    id: 295486,
-    growth: {
-    temperature_minimum: {
-    deg_f: null,
-    deg_c: null
-    },
-    shade_tolerance: null,
-    salinity_tolerance: null,
-    root_depth_minimum: {
-    inches: null,
-    cm: null
-    },
-    resprout_ability: null,
-    precipitation_minimum: {
-    inches: null,
-    cm: null
-    },
-    precipitation_maximum: {
-    inches: null,
-    cm: null
-    },
-    planting_density_minimum: {
-    sqm: null,
-    acre: null
-    },
-    planting_density_maximum: {
-    sqm: null,
-    acre: null
-    },
-    ph_minimum: null,
-    ph_maximum: null,
-    moisture_use: null,
-    hedge_tolerance: null,
-    frost_free_days_minimum: null,
-    fire_tolerance: null,
-    fertility_requirement: null,
-    drought_tolerance: null,
-    cold_stratification_required: null,
-    caco_3_tolerance: null,
-    anaerobic_tolerance: null
-    },
-    fruit_or_seed: {
-    seed_persistence: null,
-    seed_period_end: null,
-    seed_period_begin: null,
-    seed_abundance: null,
-    conspicuous: null,
-    color: null
-    },
-    foliage: {
-    texture: null,
-    porosity_winter: null,
-    porosity_summer: null,
-    color: null
-    },
-    flower: {
-    conspicuous: null,
-    color: null
-    },
-    family_common_name: null,
-    duration: null,
-    complete_data: false,
-    common_name: null,
-    bibliography: null,
-    author: "Spreng."
-    },
-    id: 295486,
-    hybrids: [ ],
-    genus: {
-    slug: "rhipsalis",
-    name: "Rhipsalis",
-    link: "http://trefle.io/api/genuses/5848",
-    id: 5848
-    },
-    forms: [ ],
-    family_common_name: null,
-    family: {
-    slug: "cactaceae",
-    name: "Cactaceae",
-    link: "http://trefle.io/api/families/14",
-    id: 14,
-    common_name: "Trefle family"
-    },
-    duration: null,
-    division: {
-    slug: "magnoliophyta",
-    name: "Magnoliophyta",
-    link: "http://trefle.io/api/divisions/1",
-    id: 1
-    },
-    cultivars: [ ],
-    common_name: null,
-    class: {
-    slug: "magnoliopsida",
-    name: "Magnoliopsida",
-    link: "http://trefle.io/api/division_classes/1",
-    id: 1
-    }
-    }
+    "wateringNeeds": "every 3-4 weeks",
+    "sunlightNeeds": "partial sunlight",
+    "flowerColor": "white",
+    "plantGrowsUp": [{ "tall": "24″ (61 cm)", "wide": "5′ (1.5 m)" }],
+    "cold": "Not cold hardy",
+    "propagation": "by cuttings",
+    "toxicity": "non-toxic to humans and animals",
+    "activelyGrows": "Spring and Fall"
+  },
